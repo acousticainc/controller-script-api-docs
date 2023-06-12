b812fcb2a8920ebc2a680e97e0913a90b134a3ac
cherryLutz <danny@cherryaudio.com>
2023-06-12 13:34:23 -0500

    fix baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://acousticainc.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Mixcraft-10-API-docs/',
+  baseUrl: '/controller-script-api-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
